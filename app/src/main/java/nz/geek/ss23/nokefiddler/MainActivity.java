698125a50c421f9b31090f45793f05076dd78114
Stephen Shkardoon <ss23@ss23.geek.nz>
2016-02-05 18:57:46 +1300

    Further investigation functions/methods

--- app/src/main/java/nz/geek/ss23/nokefiddler/MainActivity.java
+++ app/src/main/java/nz/geek/ss23/nokefiddler/MainActivity.java
@@ -1,15 +1,89 @@
 package nz.geek.ss23.nokefiddler;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Set<BluetoothDevice> pairedDevices;
+    private BluetoothAdapter BA;
+    private UUID myUUID;
+
+    private static UUID RX_SERVICE_UUID = UUID.fromString("1bc50001-0200-d29e-e511-446c609db825");
+    private static UUID RX_CHAR_UUID = UUID.fromString("1bc50002-0200-d29e-e511-446c609db825");
+    private static UUID TX_CHAR_UUID = UUID.fromString("1bc50003-0200-d29e-e511-446c609db825");
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Log.d("BTLOL", "Found a new device: " + device.getName() + " - " + device.getAddress());
+                if (device.getName() != null && device.getName().startsWith("NOKE")) {
+                    Log.d("BTLOL", "It's a noke device!");
+                    device.connectGatt(MainActivity.this, false, mGattCallback);
+                }
+            }
+        }
+    };
+
+    private final BluetoothGattCallback mGattCallback = new BluetoothGattCallback() {
+        public void onCharacteristicChanged(BluetoothGatt paramAnonymousBluetoothGatt, BluetoothGattCharacteristic paramAnonymousBluetoothGattCharacteristic) {
+        }
+
+        public void onCharacteristicRead(BluetoothGatt paramAnonymousBluetoothGatt, BluetoothGattCharacteristic paramAnonymousBluetoothGattCharacteristic, int paramAnonymousInt)
+        {
+        }
+
+        @Override
+        public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                Log.d("BTLOL", "Discovered services");
+                List<BluetoothGattService> services = gatt.getServices();
+                for (BluetoothGattService gservice : services) {
+                    Log.d("BTLOL", "Service Instance ID" + gservice.getInstanceId() + " of type " + gservice.getType() + ", UUID " + gservice.getUuid());
+                    for (BluetoothGattCharacteristic gattchar : gservice.getCharacteristics()) {
+                        Log.d("BTLOL", "GattCharacteristic z
+                    }
+                }
+            } else {
+                Log.d("BTLOL", "Failed to discover services :(");
+            }
+        }
+
+        public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+            if (newState == BluetoothGatt.STATE_CONNECTED) {
+                Log.d("BTLOL", "Connected sucessfully");
+                gatt.discoverServices();
+            } else {
+                Log.d("BTLOL", "Failed to connect :(");
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +100,22 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Log.d("BTLOL", "Init");
+
+        myUUID = UUID.fromString("ba293d54-4a46-4879-9f87-0e72b52cf8bf");
+
+        BA = BluetoothAdapter.getDefaultAdapter();
+
+        Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        startActivityForResult(turnOn, 0);
+
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+
+        BA.startDiscovery();
+
     }
 
     @Override
